3066980e480f67098d99b0292daf078de7f14da7
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-08-25 14:17:33 +0300

    added dashboard links and active links

--- src/app/(components)/sidebar/index.tsx
+++ src/app/(components)/sidebar/index.tsx
@@ -1,46 +1,99 @@
-'use client'
+"use client";
 
 import { useAppDispatch, useAppSelector } from "../../../app/redux";
-import { Menu } from "lucide-react";
-import{setIsSidebarCollapsed} from '../../state'
+import { Archive, CircleDollarSign, Clipboard, Layout, LucideIcon, Menu, SlidersHorizontal, User } from "lucide-react";
+import { setIsSidebarCollapsed } from "../../state";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+interface sideBarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SideBarLinks = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: sideBarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname == "/" && href === "/dashboard");
+
+  return (
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center ${
+          isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+        } hover: text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? "bg-blue-200 text-white":""}`}
+      >
+        <Icon className="w-6 h-6 !text-gray-700" />
+        <span className={`${isCollapsed ? "hidden": "block"} font-medium text-gray-700`}>
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 const Sidebar = () => {
-
   const dispatch = useAppDispatch();
   const isSideBarCollapsed = useAppSelector(
     (state) => state.global.isSideBarCollapsed
-  )
+  );
 
-  const toggleSideBar = () =>{
-    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed))
-  }
+  const toggleSideBar = () => {
+    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+  };
 
   const sidebarClassNames = `fixed flex flex-col ${
     isSideBarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
-  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
 
+  return (
+    <div className={sidebarClassNames}>
+      {/* Top Logo */}
+      <div
+        className={`flex  gap-3 justify-between md:justify-normal items-center pt-8 ${
+          isSideBarCollapsed ? "px-5" : "px-8"
+        }`}
+      >
+        <div>Logo</div>
+        <h1
+          className={`${
+            isSideBarCollapsed ? "hidden" : "block"
+          } font-extrabold text-2xl`}
+        >
+          Stock
+        </h1>
 
-  return <div className={sidebarClassNames}>
-    {/* Top Logo */}
-    <div className="flex  gap-3 justify-between md:justify-normal items-center pt-8">
-      <div>Logo</div>
-      <h1 className="font-extrabold text-2xl">Stock</h1>
-    
-    <button className="rounded-full bg-gray-100 md:hidden px-3 py-3 hover:bg-blue-100" onClick={toggleSideBar}>
-      <Menu className="w-4 h-4"/>
-    </button>
+        <button
+          className="rounded-full bg-gray-100 md:hidden px-3 py-3 hover:bg-blue-100"
+          onClick={toggleSideBar}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+      {/* Links */}
+      <div className="flex-grow mt-8">
+        <SideBarLinks href="/dashboard" icon={Layout} label = "dashboard" isCollapsed={isSideBarCollapsed} />
+        <SideBarLinks href="/inventory" icon={Archive} label = "Inventory" isCollapsed={isSideBarCollapsed} />
+        <SideBarLinks href="/products" icon={Clipboard} label = "Products" isCollapsed={isSideBarCollapsed} />
+        <SideBarLinks href="/users" icon={User} label = "Users" isCollapsed={isSideBarCollapsed} />
+        <SideBarLinks href="/settings" icon={SlidersHorizontal} label = "Settings" isCollapsed={isSideBarCollapsed} />
+        <SideBarLinks href="/expenses" icon={CircleDollarSign} label = "Expenses" isCollapsed={isSideBarCollapsed} />
+      </div>
+
+      {/* Footer */}
+
+      <div>
+        <p className="text-center text-xs text-gray-500">&copy;2024 Glitch</p>
+      </div>
     </div>
-    {/* Links */}
-    <div className="flex-grow mt-8">
-
-    </div>
-
-    {/* Footer */}
-
-    <div>
-      <p className="text-center text-xs text-gray-500">&copy;2024 Glitch</p>
-    </div>
-  </div>;
+  );
 };
 
 export default Sidebar;
